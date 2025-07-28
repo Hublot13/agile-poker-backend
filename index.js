b4cbfc3f5e2d1becb175efaf95f01f86198bfa38
Shivraj <shivraj07112002@gmail.com>
2025-07-28 16:25:42 +0530

    change cors

--- index.js
+++ index.js
@@ -12,16 +12,19 @@ const app = express();
 const server = createServer(app);
 const io = new Server(server, {
   cors: {
-    origin:
-      process.env.NODE_ENV === "production"
-        ? "https://agileplanning.netlify.app/"
-        : "http://localhost:5173",
-
+    origin: process.env.CLIENT_URL,
     methods: ["GET", "POST"],
   },
 });
 
-app.use(cors());
+app.use(
+  cors({
+    origin: process.env.CLIENT_URL,
+    methods: ["GET", "POST"],
+    credentials: true,
+  })
+);
+
 app.use(express.json());
 
 mongoose
